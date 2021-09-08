00d1442cb8139d0f7907c7117220c8a0c509584d
Tyler Smith <mrtbird539@gmail.com>
2021-09-08 19:59:36 -0400

    Updated Readme

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const PORT = process.argv.env || 3001;
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
